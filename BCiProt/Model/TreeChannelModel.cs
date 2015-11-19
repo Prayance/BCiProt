fffe7c0c7b103fd10ba532f2ee754ccccdd7a877
Elisavet Andrikopoulou <elly.andrikopoulou@live.co.uk>
2015-11-19 17:07:54 +0000

    audio profile basic data, popup window

---
+++ BCiProt/Model/TreeChannelModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCiProt.Model
+{
+    public class TreeChannelModel : TreeViewItemBase
+    {
+        public TreeChannelModel()
+        {
+            this.BasicNodes = new ObservableCollection<ChannelBasicType>();
+            this.ExtraNodes = new ObservableCollection<ChannelExtraType>();
+        }
+
+        public string Name { get; set; }
+        public ObservableCollection<ChannelBasicType> BasicNodes { get; set; }
+        public ObservableCollection<ChannelExtraType> ExtraNodes { get; set; }
+
+
+        public class ChannelBasicType
+        {
+            public string basicType { get; set; }
+            public string basicValue { get; set; }
+        }
+
+
+        public class ChannelExtraType
+        {
+            public string extraType { get; set; }
+            public string extraValue { get; set; }
+            public string Mode { get; set; }
+
+            /// <summary>
+            /// If the mode is AC3 then the string will have AC3 
+            /// otherwise the string will have the NAME of the other mode.
+            /// </summary>
+            /// <returns>The Type of the Mode AC3 or other.</returns>
+            public string getModeType()
+            {
+                if (Mode == "AC3")
+                {
+                    return "AC3";
+                }
+                return "Other";
+            }
+        }
+    }
+}
